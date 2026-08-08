d574fb5ab0e59f5331395f2928681f685cf2c9fe
gobi2001-2006 <gobika495@gmail.com>
2026-08-08 23:39:58 +0530

    fixed import and build issue

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';;
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { AppContext } from '../context/AppContext';
 import { toast } from 'react-toastify';
