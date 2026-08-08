aee4c3ad40a3b947e940d5b85093b1e1ce350477
gobi2001-2006 <gobika495@gmail.com>
2026-08-08 22:48:06 +0530

    fix case-sensitive+bug import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,41 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Doctors from './pages/Doctors';
-import Profile from './pages/Profile';
-import MyAppointments from './pages/Myappointments';
-import Appointment from './pages/Appointment';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Login from './pages/Login.jsx';
+import Doctors from './pages/Doctors.jsx';
+import Profile from './pages/Profile.jsx';
+import MyAppointments from './pages/MyAppointments.jsx';
+import Appointment from './pages/Appointment.jsx';
+
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
-      <ToastContainer/>
-      <Navbar/>
+    <>
+      <Navbar />
+
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/doctors' element={<Doctors/>}/>
-        <Route path='/doctors/:speciality' element={<Doctors/>}/>
-        <Route path='/my-profile' element={<Profile/>}/>
-        <Route path='/my-appointment' element={<MyAppointments/>}/>
-        <Route path='/appointment/:docId' element={<Appointment/>}/>
-        
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/my-profile' element={<Profile />} />
+        <Route path='/my-appointment' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
-      <Footer/>
-</div>
+
+      <Footer />
+      <ToastContainer />
+    </>
   )
 }
 
