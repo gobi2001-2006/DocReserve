7c5fbda9a69346a0fa1ee32bdfcd2e226dc9a859
gobi2001-2006 <gobika495@gmail.com>
2026-08-08 23:13:45 +0530

    Fix frontend deployment issues

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { assets } from '../assets/assets;'
-
+import { assets } from "../assets/assets";
 const About = () => {
 
   const [activeIndex, setActiveIndex] = useState(0)
